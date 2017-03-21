65b2c718e29d36c4e87073e7892148a527adc9c0
ozgun-kara <ozgunkara_92@hotmail.com>
2017-03-21 20:22:44 +0200

    Update and rename 7 (char, Ve, Veya).c to 007 (char, Ve, Veya).c

--- 007 (char, Ve, Veya).c
+++ 007 (char, Ve, Veya).c
@@ -5,7 +5,7 @@ int main()
 	char tus;	
 	printf("Bir tusa basiniz:\n");
 	scanf("%c",&tus);
-	if ((65<=tus && tus<=90) || (97<=tus && tus<=122))
+	if ((65<=tus && tus<=90) || (97<=tus && tus<=122))	//	ASCII Kodları
 		printf("Harfe bastiniz\n");
 	else
 		printf("Harfe basmadiniz\n");	
