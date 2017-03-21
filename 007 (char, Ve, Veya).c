298e3f325d217990de764db4652598024ad0cd36
ozgun-kara <ozgunkara_92@hotmail.com>
2017-03-21 20:38:24 +0200

    Update 007 (char, Ve, Veya).c

--- 007 (char, Ve, Veya).c
+++ 007 (char, Ve, Veya).c
@@ -5,7 +5,7 @@ int main()
 	char tus;	
 	printf("Bir tusa basiniz:\n");
 	scanf("%c",&tus);
-	if ((65<=tus && tus<=90) || (97<=tus && tus<=122))	//	ASCII Kodları
+	if ((65<=tus && tus<=90) || (97<=tus && tus<=122))	// ASCII Kodları
 		printf("Harfe bastiniz\n");
 	else
 		printf("Harfe basmadiniz\n");	
