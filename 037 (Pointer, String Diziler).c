cbef9e32367b83a67435310c396d81663b35a28d
ozgun-kara <ozgunkara_92@hotmail.com>
2017-03-26 20:21:55 +0300

    Update 037 (Pointer, String Diziler).c

--- 037 (Pointer, String Diziler).c
+++ 037 (Pointer, String Diziler).c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-//	NOT: String dizilere, diğer dizilerden farklı olarak 
-//	son kutucuğa bilgisayar tarafından '\0' (null) karakteri eklenir. 
+// NOT: String dizilere, diÄŸer dizilerden farklÄ± olarak 
+// son kutucuÄŸa bilgisayar tarafÄ±ndan '\0' (null) karakteri eklenir. 
 
 int main()
 {
 	char dizi [] = "Zidane";
 	char * p;
-	p = dizi;					//	p = &dizi[0]; da yazılabilirdi. Metnin ilk karakterinin başlangıç adresi p'ye atandı.
+	p = dizi;			// p = &dizi[0]; da yazÄ±labilirdi. Metnin ilk karakterinin baÅŸlangÄ±Ã§ adresi p'ye atandÄ±.
 	printf("%s \n",p);
 	printf("%s \n",p+1);	
 }
