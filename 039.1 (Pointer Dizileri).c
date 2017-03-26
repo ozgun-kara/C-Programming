d7370869c8cf51d3b8f6ecc392afb40524027fd3
ozgun-kara <ozgunkara_92@hotmail.com>
2017-03-26 20:17:37 +0300

    Update 039.1 (Pointer Dizileri).c

--- 039.1 (Pointer Dizileri).c
+++ 039.1 (Pointer Dizileri).c
@@ -8,12 +8,12 @@ char * gun_dondur (char * dizi [], int hangigun)
 int main()
 {
 	char * gunler [7] = {"Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar"};
-	//	Burada char içerikli başlangıç adreslerinden 7 adet içeren gunler pointer dizisi yarattık.
-	// 	Not:	char * gunler [7];
-	//			gunler [7] = {"Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar"}; yazılamaz.
+	// Burada char iÃ§erikli baÅŸlangÄ±Ã§ adreslerinden 7 adet iÃ§eren gunler pointer dizisi yarattÄ±k.
+	// Not:	char * gunler [7];
+	// gunler [7] = {"Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi","Pazar"}; yazÄ±lamaz.
 	
-	printf("%s \n",gunler [0]);	// pointer array olan gunler'in 0. indeksindeki "Pazartesi" metninde,
-								// '\n' (null) karakterini görene kadar yazdırdı.
-	printf("%s",gun_dondur(gunler,2));	//	printf("%s",gun_dondur(gunler[7], 2)); kodu çalışmaz.
+	printf("%s \n",gunler [0]);		// pointer array olan gunler'in 0. indeksindeki "Pazartesi" metninde,
+						// '\n' (null) karakterini gÃ¶rene kadar yazdÄ±rdÄ±.
+	printf("%s",gun_dondur(gunler,2));	// printf("%s",gun_dondur(gunler[7], 2)); kodu Ã§alÄ±ÅŸmaz.
 
 }
