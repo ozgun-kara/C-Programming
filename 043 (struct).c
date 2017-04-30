9fa04fc7bb79ef6bb86f6dfc0b533fa2367b749c
ozgun-kara <ozgunkara_92@hotmail.com>
2017-04-30 22:25:39 +0300

    Update 043 (struct).c

--- 043 (struct).c
+++ 043 (struct).c
@@ -9,8 +9,8 @@ struct Futbolcu
 
 int main()
 {
-	struct Futbolcu F1;	// F1 isimli Futbolcu nesnesi yaratıldı ama bilgileri girilmedi.
-	struct Futbolcu F2 = {"Zidane",10,30};	//	struct Futbolcu F2 = {10,"Zidane",30}; şeklinde içerik sırasını değiştirip yazamayız.
+	struct Futbolcu F1;			// F1 isimli Futbolcu nesnesi yaratÄ±ldÄ± ama bilgileri girilmedi.
+	struct Futbolcu F2 = {"Zidane",10,30};	// struct Futbolcu F2 = {10,"Zidane",30}; ÅŸeklinde iÃ§erik sÄ±rasÄ±nÄ± deÄŸiÅŸtirip yazamayÄ±z.
 	printf("%d ",F2.numara);
 	printf("%s ",F2.isim);	
 }
