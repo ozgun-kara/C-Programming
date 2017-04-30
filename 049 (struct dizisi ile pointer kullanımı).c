7e75987a9ce983e82376ef93a5023faa4d9300aa
ozgun-kara <ozgunkara_92@hotmail.com>
2017-04-30 22:31:07 +0300

    Update 049 (struct dizisi ile pointer kullanımı).c

--- 049 (struct dizisi ile pointer kullanımı).c
+++ 049 (struct dizisi ile pointer kullanımı).c
@@ -13,13 +13,13 @@ int main()
 	struct ogrenci ogr [3] = {{"Ali","Kamu Yonetimi",3,2.25},{"Cem","Iktisat",2,3.10},{"Mert","Gida Muh.",1,2.80}};	
 				     	  
 	struct ogrenci * ogrptr = ogr;	// struct ogrenci tipindeki ogr dizisinin adresini tutan bir 
-									// struct ogrenci tipinde ogrptr pointer dizisi yaratýldý.
+					// struct ogrenci tipinde ogrptr pointer dizisi yaratildi.
 	for (int i=0; i<3; i++)
 	{
 		printf("Ad:	%s\n", ogrptr -> ad);
 		printf("Bolum:	%s\n", ogrptr -> bolum);
 		printf("Sinif:	%d\n", ogrptr -> sinif);
 		printf("Ortalama:	%.2f\n\n", ogrptr -> ort);
-		* ogrptr ++;	// Burada struct ogrenci tipinde ogrptr pointer dizisinin indeksi 1 artýyor.
+		* ogrptr ++;	// Burada struct ogrenci tipinde ogrptr pointer dizisinin indeksi 1 artiyor.
 	}	
 }
